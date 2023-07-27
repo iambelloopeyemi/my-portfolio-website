c506a2d813f9bfb5945315c622f0a6208ddcb2d3
iambelloopeyemi <opeyemiemmanuelbello@gmail.com>
2023-07-27 02:37:43 +0100

    implemented responsiveness on the contact section

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -47,80 +47,74 @@ export default function Contact() {
   };
 
   return (
-    <section className="bg-anti-flash-white px-20 pt-32 pb-16">
+    <section className="bg-anti-flash-white px-4 sm:px-20 pt-32 pb-16">
       <header className="section-header">
         <h1 className="heading">contact</h1>
         <Underline />
-        <h5 className="subheading">
+        <h5 className="text-xl">
           Feel free to Contact me by submitting the form below and I will get
           back to you as soon as possible
         </h5>
       </header>
-      <div className="flex justify-center">
-        <Formik
-          initialValues={{
-            name: "",
-            email: "",
-            message: "",
-          }}
-          validationSchema={validationSchema}
-          onSubmit={handleSubmit}
-        >
-          <Form className="bg-white p-10 rounded drop-shadow">
-            <label className="block mb-5">
-              <span className="label">Name</span>
-              <div className="input-wrapper">
-                <Field
-                  name="name"
-                  type="text"
-                  autoComplete="off"
-                  placeholder="Enter Your Full Name"
-                  className="input"
-                />
-                <ErrorMessage name="name" component="div" className="error" />
-              </div>
-            </label>
-            <label className="block mb-5">
-              <span className="label">Email</span>
-              <div className="input-wrapper">
-                <Field
-                  name="email"
-                  type="email"
-                  autoComplete="off"
-                  placeholder="Enter Your Email"
-                  className="input"
-                />
-                <ErrorMessage name="email" component="div" className="error" />
-              </div>
-            </label>
-            <label className="block mb-5">
-              <span className="label">Message</span>
-              <div className="input-wrapper">
-                <ErrorMessage
-                  name="message"
-                  component="div"
-                  className="error"
-                />
-                <Field
-                  name="message"
-                  as="textarea"
-                  autoComplete="off"
-                  placeholder="Enter Your Message"
-                  className="block w-[45rem] h-32 bg-seasalt rounded outline-0 font-semibold"
-                />
-              </div>
-            </label>
-            <div className="flex justify-end">
-              <button
-                type="submit"
-                className="bg-violet-blue px-12 py-3 rounded hover:scale-105 text-white uppercase font-bold"
-              >
-                submit
-              </button>
+      <Formik
+        initialValues={{
+          name: "",
+          email: "",
+          message: "",
+        }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form className="bg-white p-5 sm:p-10 rounded drop-shadow">
+          <label className="block mb-5">
+            <span className="label">Name</span>
+            <div className="input-wrapper">
+              <Field
+                name="name"
+                type="text"
+                autoComplete="off"
+                placeholder="Enter Your Full Name"
+                className="input"
+              />
+              <ErrorMessage name="name" component="div" className="error" />
             </div>
-          </Form>
-        </Formik>
-      </div>
+          </label>
+          <label className="block mb-5">
+            <span className="label">Email</span>
+            <div className="input-wrapper">
+              <Field
+                name="email"
+                type="email"
+                autoComplete="off"
+                placeholder="Enter Your Email"
+                className="input"
+              />
+              <ErrorMessage name="email" component="div" className="error" />
+            </div>
+          </label>
+          <label className="block mb-5">
+            <span className="label">Message</span>
+            <div className="input-wrapper">
+              <ErrorMessage name="message" component="div" className="error" />
+              <Field
+                name="message"
+                as="textarea"
+                autoComplete="off"
+                placeholder="Enter Your Message"
+                className="block sm:w-full h-32 bg-seasalt rounded outline-0 font-semibold"
+              />
+            </div>
+          </label>
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="bg-violet-blue px-12 py-3 rounded hover:scale-105 text-white uppercase font-bold"
+            >
+              submit
+            </button>
+          </div>
+        </Form>
+      </Formik>
       <ToastContainer />
     </section>
   );
